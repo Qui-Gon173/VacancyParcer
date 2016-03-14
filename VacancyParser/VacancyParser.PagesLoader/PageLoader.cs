1b4538878c693e59c95a09fb48132c36256151b3
Qui-Gon173 <quigon173@gmail.com>
2016-03-14 19:18:15 +0300

    Use abstract class instead of interface

---
+++ VacancyParser/VacancyParser.PagesLoader/PageLoader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VacancyParser.PagesLoader
+{
+    public abstract class PageLoader
+    {
+        private static TimeWaiter TimeWaiter;
+        public string Link { get; protected set; }
+        public int WaitTime { get; set; }
+
+        public abstract void Init();
+        public abstract VacancyData[] GetVacancy();
+    }
+}
--- VacancyParser/VacancyParser.PagesLoader/VacancyData.cs
+++ VacancyParser/VacancyParser.PagesLoader/VacancyData.cs
@@ -7,5 +7,10 @@ namespace VacancyParser
 {
     public class VacancyData
     {
+        public string Job { get; set; }
+        public string Location { get; set; }
+        public double Salary { get; set; }
+        public string Sector { get; set; }
+        public float Experiance { get; set; }
     }
 }
