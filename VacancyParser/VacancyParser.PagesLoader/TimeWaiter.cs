854b19bf460d76701c7856003b70fbf5ae3056b5
Qui-Gon173 <quigon173@gmail.com>
2016-03-14 19:00:54 +0300

    commit for commit

---
+++ VacancyParser/VacancyParser.PagesLoader/TimeWaiter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VacancyParser.PagesLoader
+{
+    public class TimeWaiter
+    {
+        private DateTime _dateLastWait;
+        private object _lockObject = new object();
+        public int TimeToWaitInMs { get; set; }
+
+        public void Wait()
+        {
+            lock(_lockObject)
+            {
+                var substract = DateTime.Now.Subtract(_dateLastWait).TotalMilliseconds;
+                if(substract<TimeToWaitInMs)
+                {
+                    var lastTimeToWait = TimeToWaitInMs - (int)substract;
+                    System.Threading.Thread.Sleep(lastTimeToWait);
+                    _dateLastWait = DateTime.Now;
+                }
+            }
+        }
+    }
+}
