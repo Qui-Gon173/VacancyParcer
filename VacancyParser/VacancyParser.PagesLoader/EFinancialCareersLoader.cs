2a6770a19cb5070f5dd8cd35f0864d5809bf0617
Qui-Gon173 <quigon173@gmail.com>
2016-03-14 22:29:13 +0300

    Added EFinancialCareersLoader

---
+++ VacancyParser/VacancyParser.PagesLoader/EFinancialCareersLoader.cs
@@ -0,0 +1,106 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace VacancyParser.PagesLoader
+{
+    public class EFinancialCareersLoader : PageLoader
+    {
+        private Queue<VacancyData> _loadedData = new Queue<VacancyData>();
+        public override int WaitTime
+        {
+            get
+            {
+                return TimeWaiter.TimeToWaitInMs;
+            }
+            set
+            {
+                TimeWaiter.TimeToWaitInMs = value;
+            }
+        }
+        public override string Link
+        {
+            get
+            {
+                return @"http://www.efinancialcareers.com/jobs-Information_Technology.s019";
+            }
+        }
+
+        private void ParceVacancyList(string link)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(LoadPage(link));
+            var threadArray = doc.DocumentNode
+                .SelectNodes(".jobPreview>h2>a")
+                .Select(el => el.GetAttributeValue("href", ""))
+                .Where(el => !string.IsNullOrEmpty(el))
+                .Select(el => new Thread(() => ParceVacancy(el)))
+                .ToArray();
+            foreach (var el in threadArray)
+                el.Start();
+            while (threadArray.Any(el => el.IsAlive))
+            {
+                Thread.Sleep(WaitTime * 2);
+            }
+        }
+
+        private void ParceVacancy(string link)
+        {
+            var result = new VacancyData();
+            var doc = new HtmlDocument();
+            doc.LoadHtml(LoadPage(link));
+
+            result.Job = doc.DocumentNode
+                .SelectSingleNode(".well>h1")
+                .InnerText;
+
+            var liContent = doc.DocumentNode
+                .SelectNodes("ul.details>li");
+
+            for (var i = 0; i < liContent.Count; i++)
+            {
+                var span=liContent[i].SelectSingleNode("span");
+                switch (liContent[i].GetAttributeValue("class",""))
+                {
+                    case "salary:": result.Salary = span.InnerText.Trim(' ', ':', '\t'); break;
+                    case "location:": result.Location = span.InnerText.Trim(' ', ':', '\t'); break;
+                }
+            }
+            lock (_loadedData)
+                _loadedData.Enqueue(result);
+        }
+
+        public override void Init()
+        {
+            if (IsInited)
+                return;
+            var doc = new HtmlDocument();
+            doc.LoadHtml(LoadPage(Link));
+            var textVacancyCount = doc.DocumentNode.SelectNodes(".pagination>.pageItem")
+                .Last()
+                .InnerText;
+            var pageCount = int.Parse(textVacancyCount);
+            var threads = new Thread[pageCount];
+            for (var i = 0; i < pageCount; i++)
+            {
+                var link = new StringBuilder(Link);
+                link.AppendFormat("?page={0}", i + 1);
+                threads[i] = new Thread(() => ParceVacancyList(link.ToString()));
+                threads[i].Start();
+            }
+            while (threads.Any(el => el.IsAlive))
+            {
+                Thread.Sleep(WaitTime * 3);
+            }
+            IsInited = true;
+        }
+
+        public override VacancyData[] GetVacancy()
+        {
+            return _loadedData.ToArray();
+        }
+    }
+}
