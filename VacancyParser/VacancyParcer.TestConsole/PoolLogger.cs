a7ffa710d2358e02043c1f433221b29b0d30e29b
Qui-Gon173 <quigon173@gmail.com>
2016-03-16 10:17:42 +0300

    Add logger

---
+++ VacancyParser/VacancyParcer.TestConsole/PoolLogger.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VacancyParser.PagesLoader;
+using System.Timers;
+
+namespace VacancyParcer.TestConsole
+{
+    public class PoolLogger : ILogger
+    {
+        private enum MessageType{
+            Debug,
+            Info,
+            Error
+            }
+
+        private struct InputData
+        {
+            public string Message { get; set; }
+            public DateTime Date { get; set; }
+        }
+
+        public string DebugFile { get; private set; }
+        public string InfoFile { get; private set; }
+        public string ErrorFile { get; private set; }
+
+        private Queue<InputData> _debugQueue = new Queue<InputData>();
+        private Queue<InputData> _infoQueue = new Queue<InputData>();
+        private Queue<InputData> _errorQueue = new Queue<InputData>();
+
+        private Timer _saveTimer = new Timer(10000);
+
+        private object _saveLock = new object();
+
+        public PoolLogger(string debugFile,string infoFile,string errorFile)
+        {
+            DebugFile = debugFile;
+            InfoFile = infoFile;
+            ErrorFile = errorFile;
+            _saveTimer.Elapsed += _saveTimer_Elapsed;
+            _saveTimer.Start();
+        }
+        
+        private void SaveLog(Queue<InputData> queue,string file)
+        {
+            lock (_saveLock)
+            {
+                var debugData = queue.Select(el => string.Format("{0:G}|{1}", el.Date, el.Message));
+                System.IO.File.AppendAllLines(file, debugData);
+            }
+        }
+
+        private void _saveTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            SaveLog(_debugQueue, DebugFile);
+            SaveLog(_infoQueue, InfoFile);
+            SaveLog(_errorQueue, ErrorFile);
+        }
+
+        private void AddEvent(MessageType type,string message)
+        {
+            Queue<InputData> queue=null;
+            switch(type)
+            {
+                case MessageType.Debug:queue = _debugQueue;break;
+                case MessageType.Info: queue = _infoQueue; break;
+                case MessageType.Error: queue = _errorQueue; break;
+            }
+            lock(_saveLock)
+                queue.Enqueue(new InputData { Message = message, Date = DateTime.Now });
+        }
+
+        public void Debug(string info)
+        {
+            AddEvent(MessageType.Debug, info);
+        }
+
+        public void Debug(string format, params object[] args)
+        {
+            AddEvent(MessageType.Debug, string.Format(format,args));
+        }
+
+        public void Error(string info)
+        {
+            AddEvent(MessageType.Error, info);
+        }
+
+        public void Error(string format, params object[] args)
+        {
+            AddEvent(MessageType.Error, string.Format(format, args));
+        }
+
+        public void Info(string info)
+        {
+            AddEvent(MessageType.Info, info);
+        }
+
+        public void Info(string format, params object[] args)
+        {
+            AddEvent(MessageType.Info, string.Format(format, args));
+        }
+    }
+}
--- VacancyParser/VacancyParcer.TestConsole/Program.cs
+++ VacancyParser/VacancyParcer.TestConsole/Program.cs
@@ -10,45 +10,9 @@ namespace VacancyParcer.TestConsole
 {
     class Program
     {
-        static TimeWaiter waiter = new TimeWaiter();
-
-        static void load(object link,int _try=0)
-        {
-            if (_try > 5)
-                return;
-            try
-            {
-                waiter.Wait();
-                using (var client = new System.Net.WebClient())
-                {
-                    var data = client.DownloadString((string)link);
-                    Console.WriteLine("{0}:{1}",link,data.Length);
-                }
-            }catch(Exception e)
-            {
-                Console.WriteLine("{0}:{1}", link, e);
-                load(link,_try++);
-            }
-        }
-
         static void Main(string[] args)
         {
-            Console.SetOut(new System.IO.StreamWriter("log.txt"));
-            waiter.TimeToWaitInMs = 600;
-            var threadList = new List<Thread>();
-            for(var i=0;i<112;i++)
-            {
-                threadList.Add(new Thread(l=>load(l)) { Name=i.ToString()});
-            }
-            for (var i = 0; i < 112; i++)
-            {
-                threadList[i].Start(@"http://itmozg.ru/search/vacancy/?page=" + (i + 1));
-            }
-        
-            while(threadList.Any(el=>el.IsAlive))
-                Thread.Sleep(1000);
-            Console.WriteLine("It's all!");
-            Console.ReadKey();
+            
         }
     }
 }
