8ff2996493ce61f9db5695e3d0450d6432eb30bd
Qui-Gon173 <quigon173@gmail.com>
2016-03-16 22:13:04 +0300

    commit for delete

--- VacancyParser/VacancyParcer.TestConsole/Program.cs
+++ VacancyParser/VacancyParcer.TestConsole/Program.cs
@@ -59,25 +59,6 @@ namespace VacancyParcer.TestConsole
             {
                 vacancy = (VacancyData[])serial.Deserialize(reader);
             }
-            foreach (var x in vacancy)
-            {
-                if (x.Job != null)
-                    x.Job = PageLoader.RemoveDeviders(x.Job, deviders).Trim();
-                if (x.Location != null)
-                    x.Location = PageLoader.RemoveDeviders(x.Location, deviders).Trim();
-                if (x.Salary != null)
-                    x.Salary = PageLoader.RemoveDeviders(x.Salary, deviders).Trim();
-                if (x.Sector != null)
-                    x.Sector = PageLoader.RemoveDeviders(x.Sector,deviders).Trim();
-                if (x.Skils != null)
-                    x.Skils = PageLoader.RemoveDeviders(x.Skils, deviders).Trim();
-                if (x.Experiance != null)
-                    x.Experiance = PageLoader.RemoveDeviders(x.Experiance, deviders).Trim();
-            }
-            using (var writer = new System.IO.StreamWriter("i_data_fix.txt"))
-            {
-                serial.Serialize(writer, vacancy);
-            }
 
         }
     }
