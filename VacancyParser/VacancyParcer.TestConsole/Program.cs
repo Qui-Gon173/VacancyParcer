d9e5fef0b31b85ac22bd85507dca11124e9a1041
Qui-Gon173 <quigon173@gmail.com>
2016-03-17 23:52:29 +0300

    some shit

--- VacancyParser/VacancyParcer.TestConsole/Program.cs
+++ VacancyParser/VacancyParcer.TestConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -50,15 +51,97 @@ namespace VacancyParcer.TestConsole
             iloader.Logger.ForceSave();
         }
 
+        static bool ContainsAny(string value, params string[] array)
+        {
+            foreach (var el in array)
+                if (value.Contains(el))
+                    return true;
+            return false;
+        }
+
+
+
+        static Dictionary<string, int> Els(VacancyData[] data)
+        {
+            var types = new Dictionary<string, string[]>{
+            {"analitics", new []{ "analyst", "risk", "аналитик" }},
+            {"testers", new[]{ "тест", "delivery", "qa", "test", "validat", "тестировщик" }},
+            {"develop",new []{ "программист", "develop", "сопровожден", "бэкенд", "engineer", "java", "technology", "software", "разработчик" }},
+            {"designer",new [] { "model", "художник", "иллюстратор", "аниматор", "арт", "дизайнер" }},
+            {"admin",new []{ "электромонтажник", "сервис", "техни", "network", "админ" }},
+            {"manager",new []{ "manager", "support", "консультант", " маркетолог", "копирайтер", "наборщик", "consultant", "менеджер" }},
+            {"seo",new []{ "seo", "smm" }}
+            };
+            var result = new Dictionary<string, int>();
+            foreach (var key in types.Keys)
+                foreach (var value in types[key])
+                    foreach (var el in data)
+                        if (el.Job.Contains(value))
+                            if (!result.ContainsKey(key))
+                                result.Add(key, 1);
+                            else
+                                result[key]++;
+            return result;
+        }
+
         static void Main(string[] args)
         {
             var serial = new System.Xml.Serialization.XmlSerializer(typeof(VacancyData[]));
-            VacancyData[] vacancy;
-            var deviders = new[] { '\t', '\r', '\n' };
-            using (var reader = new System.IO.StreamReader("i_data.txt"))
+            var allVacancy = new List<VacancyData>();
+
+            foreach (var file in new[] { "allData.txt" })
+                using (var reader = new StreamReader(file))
+                {
+                    VacancyData[] vacancy;
+                    vacancy = (VacancyData[])serial.Deserialize(reader);
+                    allVacancy.AddRange(vacancy);
+                }
+
+            string[] analitics = { "analyst", "risk", "аналитик" };
+            string[] testers = { "тест", "delivery", "qa", "test", "validat", "тестировщик" };
+            string[] develop = { "программист", "develop", "сопровожден", "бэкенд", "engineer", "java", "technology", "software", "разработчик" };
+            string[] designer = { "model", "художник", "иллюстратор", "аниматор", "арт", "дизайнер" };
+            string[] admin = { "электромонтажник", "сервис", "техни", "network", "админ" };
+            string[] manager = { "manager", "support", "консультант", " маркетолог", "копирайтер", "наборщик", "consultant", "менеджер" };
+            string[] seo = { "seo", "smm" };
+
+            /*
+            var r=new Random();
+            foreach (var el in allVacancy)
+                el.Experiance = r.Next(0, 6).ToString();
+
+            using (var writer = new StreamWriter("allData.txt"))
             {
-                vacancy = (VacancyData[])serial.Deserialize(reader);
-            }
+                serial.Serialize(writer,allVacancy.ToArray());
+            }*/
+
+            var kazanVacancy = Els(allVacancy
+                .Where(el => el.Location.ToLower().Contains("казань") || el.Location.ToLower().Contains("kazan"))
+                .ToArray());
+            
+            var highlevelSalary = Els(allVacancy
+                .Where(el => int.Parse(el.Salary) > 110000)
+                .ToArray());
+
+            var moscowAndhighLevelSalary = Els(allVacancy
+                .Where(el => (el.Location.ToLower().Contains("москва")
+                    || el.Location.ToLower().Contains("moscow"))
+                    && int.Parse(el.Salary) > 90000)
+                .ToArray());
+
+            var exp2SqlCsharp = Els(allVacancy
+                .Where(el => el.Skils != null && (el.Skils.Contains("C#") &&
+                     el.Skils.ToLower().Contains("sql"))
+                    && int.Parse(el.Experiance) > 2)
+                .ToArray());
+
+            var php1c = Els(allVacancy
+                .Where(el => el.Skils != null && (el.Skils.Contains("1С") ||
+                     el.Skils.ToLower().Contains("PHP"))
+                    && int.Parse(el.Salary) > 110000)
+                .ToArray());
+
+            Console.ReadKey();
 
         }
     }
